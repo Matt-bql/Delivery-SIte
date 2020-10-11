93c0c923e61421486fb917678beb2a5ab842d7a3
bonn <mattbreaux@Holla.local>
2020-10-11 17:08:41 -0500

    new commit

---
+++ context.js
@@ -0,0 +1,39 @@
+// If your object has any non-string properties,
+// JavaScript will automatically typecast them as strings.
+var anotherObject = {
+    make: 'ford',
+    5: 'five',
+    'model': 'focus',
+    newCar: 'hello'
+};
+// ex. 1
+console.log(anotherObject.model);
+//ex. 2
+console.log(anotherObject[5]);
+//ex.3 --This can be very useful for 
+//iterating through an object's properties 
+//or when accessing a lookup table.
+var myCar = 'make';
+myType = anotherObject[myCar];
+
+console.log(myType);
+
+// function tes(mak) {
+//     var s = 'new';
+//     return s + mak;
+// }
+// var someCar = tes('Car');
+// console.log(anotherObject[someCar]);
+
+switch (1) {
+    case 1: 
+        result = '22';
+        break;
+}
+function lestt (){
+if (varrr) {
+    return 'yesss';
+} else {
+    return 'naaaahhhoo';
+}
+}
